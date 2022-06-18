3415187bbde71b8a3c988f717409b26dea5c2a26
oussamachah2020 <otaksam213@gmail.com>
2022-06-18 22:53:40 +0100

    API

---
+++ 03-task-manager/models/Task.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const TaskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'must provide name'],
+        trim: true,
+        maxlenght: [20, 'name can not be more than 20']
+    }, 
+    completed: {
+        type: Boolean,
+        default: false
+    },
+})
+
+module.exports = mongoose.model('Task', TaskSchema)
